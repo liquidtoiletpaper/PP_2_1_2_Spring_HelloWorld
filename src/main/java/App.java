eb58862e67488584464684e7ee61617699224de9
tautaste <doritos1488@gmail.com>
2024-01-30 13:00:03 +0900

    Update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean = bean2);
+        System.out.println(bean);
+        System.out.println(bean2);
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.getMessage());
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+        System.out.println(beanCat1 == beanCat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String message;
+    public Cat(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
